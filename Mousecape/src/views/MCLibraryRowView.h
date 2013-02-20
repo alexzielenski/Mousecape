a9c5144536c1ceaeb5a744491e7ed781ff0fae5e
Alex Zielenski <alex@zielenski.net>
2013-02-19 21:01:49 -0500

    Add special row view for the cape row in the editor

---
+++ Mousecape/src/views/MCLibraryRowView.h
@@ -0,0 +1,13 @@
+//
+//  MCLibraryRowView.h
+//  Mousecape
+//
+//  Created by Alex Zielenski on 2/19/13.
+//  Copyright (c) 2013 Alex Zielenski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MCLibraryRowView : NSTableRowView
+
+@end
