afbd49b99e5d213c34d302914281e976fd4f9770
Alex Zielenski <alex@zielenski.net>
2014-02-01 17:01:08 -0500

    Add MCPrefs

---
+++ Mousecape/mousecloak/MCPrefs.h
@@ -0,0 +1,13 @@
+//
+//  MCPrefs.h
+//  Mousecape
+//
+//  Created by Alex Zielenski on 2/1/14.
+//  Copyright (c) 2014 Alex Zielenski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCPrefs : NSObject
+
+@end
---
+++ Mousecape/mousecloak/MCPrefs.m
@@ -0,0 +1,13 @@
+//
+//  MCPrefs.m
+//  Mousecape
+//
+//  Created by Alex Zielenski on 2/1/14.
+//  Copyright (c) 2014 Alex Zielenski. All rights reserved.
+//
+
+#import "MCPrefs.h"
+
+@implementation MCPrefs
+
+@end
