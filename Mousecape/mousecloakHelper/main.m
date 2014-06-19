b3f56427d9f686c5582aeae461fb4b51c444d415
Alex Zielenski <alex@zielenski.net>
2014-06-19 12:39:47 -0400

    rewrite helper tool to be an app and install it per-user

---
+++ Mousecape/mousecloakHelper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mousecape
+//
+//  Created by Alexander Zielenski on 6/18/14.
+//  Copyright (c) 2014 Alex Zielenski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "listen.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        listener();
+        return EXIT_SUCCESS;
+    }
+}
